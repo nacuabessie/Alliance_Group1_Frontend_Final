dd0deb7a009e7a11db64b2078c48e14326610dd4
bessie nacua <bessie.nacua@usjr.edu.ph>
2022-11-28 17:59:32 +0800

    Installed Packages2

--- ticket-tracker/src/app/app.module.ts
+++ ticket-tracker/src/app/app.module.ts
@@ -1,17 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HttpClientModule,
+    // FormsModule,
+    // ReactiveFormsModule,
+    // MatToolbarModule,
+    // MatButtonModule,
+    // MatDialogModule,
+    // MatInputModule,
+    // HotToastModule.forRoot(),
+    // BrowserAnimationsModule,
+    // DashboardModule,
   ],
+
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule  
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
