05c658cfea52153eeed9e3c8bb22e23dd179576d
Pete Tiongzon <pete.tiongzon@gmail.com>
2022-11-28 22:20:51 +0800

    fixed the error

--- ticket-tracker/src/app/app.module.ts
+++ ticket-tracker/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
   declarations: [
